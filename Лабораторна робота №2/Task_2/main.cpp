eb66ac8b7c1f802f946465b937cd2cfbdfd6f648
Vadim <vadimvamtf@gmail.com>
2024-11-26 17:32:38 +0200

    Cleaning of unnecessary files

--- Лабораторна робота №2/Task_2/main.cpp
+++ Лабораторна робота №2/Task_2/main.cpp
@@ -4,12 +4,12 @@
 
 int main() {
 
-    Polynom polynom1("-3x^2 + 3xy^2 - 30x - 40 - 5y - 3y^2 - 15xy");
+    Polynom polynom1("-3x^2 + 3xy^2 - 3j0x - 40 - 5y - 3y^2 - 15xy");
     Polynom polynom2("x^2 - 4x^2 + 5x - 5 - 35 + y + 2y^2 + 10xy - 5xy^2");
-    Polynom polynom3("2x -x");
+    Polynom polynom3("2x -4");
     Polynom polynom4("x + 1");
 
-    string resultAdd = solutionPolynomials(polynom3, '+', polynom4);
+    string resultAdd = solutionPolynomials(polynom1, '+', polynom2);
     cout << "result add : " << resultAdd << endl;
     string resultMultiply = solutionPolynomials(polynom3, '*', polynom4);
     cout << "result multiply : " << resultMultiply << endl;
